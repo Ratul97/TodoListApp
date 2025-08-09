71e75e5ae711f7d08d153c37f1d0fac74ccc9a8e
Shakawat_Ratul <62506442+Ratul97@users.noreply.github.com>
2025-08-10 01:58:37 +0600

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Kazi Shakawat Hossain Ratul on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
